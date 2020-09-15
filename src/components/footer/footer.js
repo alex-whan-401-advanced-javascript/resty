2cdf784ef4d85724e4b8af220adb777c83e5c6d2
Alexander Whan <whan.alexander@gmail.com>
2020-09-14 21:18:18 -0700

    Added basic styling for Header, Form, and Footer components

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './footer.scss';
 
 function Footer() {
   return <footer>&copy; 2020 Alex Whan</footer>;
--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './form.scss';
 
 class Form extends React.Component {
   constructor(props) {
@@ -78,7 +79,7 @@ class Form extends React.Component {
           />
           <label>DELETE</label>
         </form>
-        <div className="results">
+        <div id="results">
           <h3>
             {this.state.method} {this.state.url}
           </h3>
