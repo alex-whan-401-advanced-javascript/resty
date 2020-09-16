9a8cdb0710978d6240f416c4da7b8aed55433643
Alexander Whan <whan.alexander@gmail.com>
2020-09-15 17:49:07 -0700

    Refactored App to Class and tasked form with pulling API data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import './App.css';
-import Header from '../src/components/header/header';
-import Form from '../src/components/form/form';
-import Footer from '../src/components/footer/footer';
+import Header from './components/header/Header';
+import Form from './components/form/Form';
+import Results from './components/results/Results';
+import Footer from './components/footer/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Form />
-      <Footer />
-    </div>
-  );
+//Container
+
+//Holds state: Count and Results Array
+
+//A class method that can update state
+
+//Renders 2 Child Components
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      headers: null,
+      results: [],
+    };
+  }
+
+  // Missing headers
+  // https://swapi.dev/api/people/
+  handleForm = (count, headers, results) => {
+    this.setState({ count, headers, results });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Form prompt="Go!" handler={this.handleForm} />
+        <Results
+          count={this.state.count}
+          headers={this.state.headers}
+          results={this.state.results}
+        />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
